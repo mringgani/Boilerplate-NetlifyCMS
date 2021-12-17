e748adb5076e9250b881892ed25838d416ffb832
mringgani <88936159+mringgani@users.noreply.github.com>
2021-12-17 09:12:39 +0700

    Update Footer.js

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = props => (
   <div className="container">
     <div className="d-flex justify-content-center" style={{ margin: "2rem" }}>
-      Copyright 2021 {props.copyright}
+      Copyright 2021 {props.copyright} - [HariAsia](https://hariasia.co.vu)
     </div>
   </div>
 )
