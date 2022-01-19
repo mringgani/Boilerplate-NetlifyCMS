82062a3da07711817e7a68d830c355ceb42384df
mringgani <88936159+mringgani@users.noreply.github.com>
2022-01-19 14:32:49 +0700

    Update Footer.js

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = props => (
   <div className="container">
     <div className="d-flex justify-content-center" style={{ margin: "2rem" }}>
-      Copyright 2021 {props.copyright} - <a href='https://cantik.neocities.org' target='_blank'>Cantik</a> 
+      Copyright 2021 {props.copyright} - <a href='https://cantik.neocities.org' title='Cantik' target='_blank'>Cantik</a> - <a href='https://hariasia.co.vu' title='HariAsia' target='_blank'>HariAsia</a> - <a href='https://brajag.netlify.app/type-approval-indonesia/' title='Type Approval Indonesia' target='_blank'>Type Approval Indonesia</a> 
     </div>
   </div>
 )
